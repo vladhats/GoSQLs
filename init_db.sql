00895ce11862a4f167b0e7102b2c6eba965118f7
vladhats <hatsvladyslav@gmail.com>
2023-04-04 02:20:54 +0300

    first population !!!

--- init_db.sql
+++ init_db.sql
@@ -1,12 +1,12 @@
 CREATE TABLE worker(
     id INT AUTO_INCREMENT PRIMARY KEY, 
-    name VARCHAR(1000) VARCHAR(1000) CHECK (LENGTH(name)>= 2) NOT NULL,
-     birthday DATE CHECK (birthday>'1900-01-01'), 
+    name VARCHAR(1000) NOT NULL CHECK (LENGTH(name)>= 2) ,
+     birthday DATE CHECK (birthday BETWEEN'1900-01-01' AND CURRENT_DATE), 
      level ENUM ('Junior', 'Trainee', 'Middle', 'Senior'),
       salary INT CHECK( salary BETWEEN 100 AND 100000) );
 CREATE TABLE client( 
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(1000) CHECK (LENGTH(name)>=2) NOT NULL
+    name VARCHAR(1000) NOT NULL CHECK (LENGTH(name)>=2) 
 );   
 CREATE TABLE project ( 
     id INT AUTO_INCREMENT PRIMARY KEY, 
@@ -16,7 +16,7 @@ CREATE TABLE project (
 );
 CREATE TABLE project_worker(
     project_id INT,
-    worker_id,
+    worker_id INT,
     FOREIGN KEY(project_id) REFERENCES project(id),
     FOREIGN KEY(worker_id) REFERENCES worker(id)
 );
---
+++ populate_db.sql
@@ -0,0 +1,12 @@
+INSERT INTO worker (name, birthday, level, salary) VALUES 
+('God Lord', '1902-01-01', 'Senior', 99000),
+('Jesus Christ', '1914-12-25', 'Senior', 99000),
+('Adam', '1950-11-11', 'Senior', 75000),
+('Eve', '1951-06-10', 'Middle', 74000),
+('Noah', '1914-10-18', 'Middle', 55000),
+('Abraham', '1965-07-15', 'Senior', 40000),
+('Joseph', '1983-02-03', 'Senior', 30000),
+('David', '1945-05-09', 'Middle', 69000),
+('Peter', '1919-08-24', 'Trainee', 6000),
+('Paul', '1999-04-16', 'Trainee',6000);
+
